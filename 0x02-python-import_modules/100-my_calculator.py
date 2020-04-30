40f5dd5e2be4c3a6b5a2f90352a9d4169fc824fa
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-04-30 19:30:37 +0000

    imports add, sub, mul and div from the file calculator_1.py and handles basics operations

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
+
+long = len(sys.argv)
+
+if long != 4:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    exit(1)
+
+a = int(sys.argv[1])
+b = int(sys.argv[3])
+
+if sys.argv[2] == "+":
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+elif sys.argv[2] == "-":
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+elif sys.argv[2] == '*':
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+elif sys.argv[2] == "/":
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+else:
+    print("unknown operator. Available operators: +, -, * and /")
+    exit(1)
