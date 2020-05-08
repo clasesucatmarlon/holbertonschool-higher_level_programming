34c9812dfca8afb802e19d0f40295ff4ebd2fbb3
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-05-08 18:10:23 +0000

    Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,30 +1,23 @@
 #!/usr/bin/python3
+def get_roman_value(char=''):
+    dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    return dic[char]
+
+
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str):
-        return 0
     if not roman_string.isupper():
         return 0
     if not roman_string:
         return 0
-    dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    sum, i = 0, 0
-    if len(roman_string) == 1:
-        return dic[roman_string[0]]
-    p = 0
-    while i < len(roman_string) - 1:
-        aux1 = dic[roman_string[i]]
-        aux2 = dic[roman_string[i + 1]]
-        if aux1 == aux2:
-            if p <= 3:
-                sum += aux2 + aux1
-                i += 1
-                p += 1
+    if isinstance(roman_string, str):
+        aux = list(map(get_roman_value, list(roman_string)))
+        sum = 0
+        for i in range(0, len(aux) - 1):
+            if aux[i] < aux[i + 1] and i > 0:
+                sum -= aux[i]
             else:
-                return 0
-        elif aux1 < aux2:
-            sum += aux2 - aux1
-            i += 1
-        else:
-            sum += aux1
-        i = i + 1
-    return sum
+                sum += aux[i]
+        sum += aux[len(aux) - 1]
+        return sum
+    else:
+        return 0
