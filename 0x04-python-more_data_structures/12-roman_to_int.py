df15f18c0ec431b102f6fe74fe45c55c9f3d0d8b
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-05-08 23:13:56 +0000

    Work numbers romans

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
-def get_roman_value(char=''):
-    dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    return dic[char]
-
-
 def roman_to_int(roman_string):
     if not roman_string or type(roman_string) != str:
         return 0
-
-    aux = list(map(get_roman_value, list(roman_string)))
-    print("aux = {} ".format(aux))
+    dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    aux = roman_string
     sum = 0
-    for i in range(0, len(aux)):
-        if aux[i] > aux[i - 1] and i > 0:
-            sum += aux[i] - (2 * aux[i - 1])
+    for i in range(len(roman_string)):
+        if dic[aux[i]] > dic[aux[i - 1]] and i > 0:
+            sum += dic[aux[i]] - (2 * dic[aux[i - 1]])
         else:
-            sum += aux[i]
+            sum += dic[aux[i]]
     return sum
