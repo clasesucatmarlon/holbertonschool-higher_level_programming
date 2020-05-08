8a5282d87363b4ecf232ac92bbce391ebb0e05e4
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-05-08 00:56:58 +0000

    Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     if not isinstance(roman_string, str):
         return 0
 
@@ -9,7 +8,7 @@ def roman_to_int(roman_string):
 
     if not roman_string:
         return 0
-
+    dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     sum, i = 0, 0
     if len(roman_string) == 1:
         return dic[roman_string[0]]
