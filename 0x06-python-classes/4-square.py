38c15a17a6883b9e19c06119a32df8b1b43ec61d
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-05-19 21:22:12 +0000

    Write a class Square that defines a square by:based on 3-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,17 +1,36 @@
 #!/usr/bin/python3
+"4-square.py define"
+
+
 class Square:
+    """class Square
+    """
+
     def __init__(self, size=0):
+        """Inizialitation of variables
+        Arg self identificador
+        size tamañe of square
+        """
         self.size = size
 
     def area(self):
-        return self.__size ** 2
+        """Inizialitation of variables
+        Arg self identificador
+        """ return self.__size ** 2
 
     @property
     def size(self):
+        """Inizialitation of variables
+        Arg self identificador
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """Inizialitation of variables
+        Arg self identificador
+        value of square
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
