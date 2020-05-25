0a20e39f46002c23d2f4f43c8f9ab55395d48ba2
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-05-25 21:43:56 +0000

    Write a class Rectangle that defines a rectangle by:based on 4-rectangle.py

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -14,8 +14,8 @@ class Rectangle:
             width {int} -- [description] (default: {0})
             height {int} -- [description] (default: {0})
         """
-        self.width = width
         self.height = height
+        self.width = width
 
     @property
     def width(self):
@@ -75,7 +75,7 @@ class Rectangle:
         Returns:
             [type] -- [area]
         """
-        return self.width * self.height
+        return self.__width * self.__height
 
     def perimeter(self):
         """[summary]
@@ -83,9 +83,9 @@ class Rectangle:
         Returns:
             [type] -- [perimeter]
         """
-        if self.width == 0 or self.height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
-        return (self.width * 2) + (self.height * 2)
+        return (self.__width * 2) + (self.__height * 2)
 
     def __str__(self):
         """[summary]
@@ -93,18 +93,18 @@ class Rectangle:
         Returns:
             [type] -- [rectangle]
         """
-        if self.width == 0 or self.height == 0:
+        if self.__width == 0 or self.__height == 0:
             return ""
-        size = "#" * self.width
-
-        return (size + "\n") * self.height
+        size = "#" * self.__width
+        string = ""
+        for i in range(self.__height):
+            if i == self.__height - 1:
+                string += size
+            else:
+                string += (size + "\n")
+        return string
 
     def __repr__(self):
-        """[summary]
-
-        Returns:
-            [type] -- [print widht anf hight]
-        """
         return ("Rectangle({:d}, {:d})".format(self.__width, self.__height))
 
     def __del__(self):
