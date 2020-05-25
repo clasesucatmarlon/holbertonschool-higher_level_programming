414f243591415e576e58c7e674362fa4f00e9314
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-05-25 23:51:28 +0000

    Write a class Rectangle that defines a rectangle by:based on 5-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -42,7 +42,7 @@ class Rectangle:
         if type(value) not in [int]:
             raise TypeError("width must be an integer")
         if value < 0:
-            raise TypeError("width must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -68,7 +68,7 @@ class Rectangle:
         if type(value) not in [int]:
             raise TypeError("height must be an integer")
         if value < 0:
-            raise TypeError("height must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
