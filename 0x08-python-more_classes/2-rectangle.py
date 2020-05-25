318d526a06ca30ba5f30d92a1551dad8b0267800
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-05-25 21:43:20 +0000

    Write a class Rectangle that defines a rectangle by:based on 1-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -14,8 +14,8 @@ class Rectangle:
             width {int} -- [description] (default: {0})
             height {int} -- [description] (default: {0})
         """
-        self.width = width
         self.height = height
+        self.width = width
 
     @property
     def width(self):
@@ -75,7 +75,7 @@ class Rectangle:
         Returns:
             [type] -- [area]
         """
-        return self.width * self.height
+        return self.__width * self.__height
 
     def perimeter(self):
         """[summary]
@@ -83,6 +83,6 @@ class Rectangle:
         Returns:
             [type] -- [perimeter]
         """
-        if self.width == 0 or self.height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
-        return (self.width * 2) + (self.height * 2)
+        return (self.__width * 2) + (self.__height * 2)
