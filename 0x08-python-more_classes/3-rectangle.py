619b847106480b406823feee9d8737fafb63e64d
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-05-25 23:45:52 +0000

    Write a class Rectangle that defines a rectangle by:based on 2-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -40,7 +40,7 @@ class Rectangle:
         if type(value) not in [int]:
             raise TypeError("width must be an integer")
         if value < 0:
-            raise TypeError("width must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -66,7 +66,7 @@ class Rectangle:
         if type(value) not in [int]:
             raise TypeError("height must be an integer")
         if value < 0:
-            raise TypeError("height must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
@@ -75,7 +75,7 @@ class Rectangle:
         Returns:
             [type] -- [area]
         """
-        return self.width * self.height
+        return self.__width * self.__height
 
     def perimeter(self):
         """[summary]
@@ -83,9 +83,9 @@ class Rectangle:
         Returns:
             [type] -- [perimeter]
         """
-        if self.width == 0 or self.height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
-        return (self.width * 2) + (self.height * 2)
+        return (self.__width * 2) + (self.__height * 2)
 
     def __str__(self):
         """[summary]
