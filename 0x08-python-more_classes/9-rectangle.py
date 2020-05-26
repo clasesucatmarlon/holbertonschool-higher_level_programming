a4c4ba02048244c4c61cc2d71f74c8b5b749a8fd
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-05-26 00:09:17 +0000

    Write a class Rectangle that defines a rectangle by:based on 8-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -154,7 +154,7 @@ class Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() = > rect_2.area():
+        if rect_1.area() == rect_2.area() or rect_1.area() > rect_2.area():
             return (rect_1)
         return rect_2
 
@@ -168,4 +168,5 @@ class Rectangle:
         Returns:
             [type] -- [description]
         """
-        return (cls(size, size))
+        cls.number_of_instances += 1
+        return (Rectangle(size, size))
