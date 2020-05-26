65af2980266245dbf1128adaafd967f60025bbc7
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-05-26 00:04:44 +0000

    Write a class Rectangle that defines a rectangle by:based on 8-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -137,14 +137,6 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-        """[summary]
-
-        Keyword Arguments:
-            size {int} -- [description] (default: {0})
-
-        Returns:
-            [type] -- [description]
-        """
         return (cls(size, size))
 
     @staticmethod
