378e1c959a969c34a829eb62ea309f3ff951b630
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-06-02 18:59:20 +0000

    class BaseGeometry (based on 5-base_geometry.py)

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""class BaseGeometry (based on 5-base_geometry.py)
+    """
+
+
+class BaseGeometry:
+    """contains area only
+    """
+
+    def area(self):
+        """nothing
+        """
+        raise Exception("area() is not implemented")
