95614e4527264233757eee009ca609e176a5cd11
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-06-02 19:06:40 +0000

    class BaseGeometry (based on 6-base_geometry.py)

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -15,7 +15,7 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """Validate values
         """
-        if type(value) in not int:
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         elif value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
