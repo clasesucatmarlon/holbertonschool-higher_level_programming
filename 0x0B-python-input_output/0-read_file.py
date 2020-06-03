3a6737c1bcdc048ef83c6b37b4d6b0d25bf6e9b7
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-06-03 22:07:15 +0000

    function that reads a text file (UTF8) and prints it to stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,6 @@
 def read_file(filename=""):
     """read text file with utf-8
     """
-    with open(filename, "r", encoding="utf-8") as data_file:
+    with open(filename, "r", encoding="utf-8") as data_read:
         data_read = file_tmp.read()
     print(data_read.rstrip())
