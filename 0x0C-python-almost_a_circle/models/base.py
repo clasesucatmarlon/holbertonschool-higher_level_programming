96a06071144e43d86ba2091739800102d6312557
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-06-06 23:48:24 +0000

    base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,9 @@
     """
 
 
+import json
+
+
 class Base:
     """Base class with methods
     """
@@ -16,3 +19,29 @@ class Base:
         else:
             type(self).__nb_objects += 1
             self.id = type(self).__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """return JSON representation of list of directories
+
+        Args:
+            list_dictionaries ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        if list_objs is None:
+            with open(cls.__name__ + ".json", "w+") as data_json:
+                data_json.write(cls.to_json_string(None))
+        else:
+            array = []
+            for obj in list_objs:
+                array.append(obj.to_dictionary())
+            with open(cls.__name__ + ".json", "w+") as data_json:
+                data_json.write(cls.to_json_string(array))
