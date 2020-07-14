c86427a6196045a70c579d51febf3e069a869c2f
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-07-14 17:14:51 +0000

    listing the privileges granted

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,7 +1,7 @@
 -- listing the privileges granted to 2 users
 
 -- user: first
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR user_0d_1@localhost;
 
 -- user: second
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR user_0d_2@localhost;
