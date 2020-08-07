e470c4c2bfd2cd7bd81c490c32392c3a0af24aa9
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-08-07 22:28:37 +0000

    task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    """ validations numeber of arguments
+    """
+    if len(argv) != 4:
+        print("Syntax: ./0-select_states.py <username> <password> <database name>")
+        exit(1)
+
+    host = 'localhost'
+    port = 3306
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    database = sys.argv[3]
+
+    cone = MySQLdb.connect(host=host, port=port, user=user, passwd=passwd, db=database)
+
+    cur = db.cursor()
+
+    cursor.execute('SELECT * FROM states ORDER BY states.id ASC')
+
+    states = cur.fechall()
+
+    for state in states:
+        print(state)
+    cur.close()
+    db.close()
