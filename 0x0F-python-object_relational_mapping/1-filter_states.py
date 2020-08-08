8175a6154cda10c52449434aa1b45377c94eab2c
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-08-07 19:42:57 -0500

    List all states with a name staring with N (upper N)

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+# List all states with a name staring with N (upper N)
+# from the database hbtn_0e_0_usa
+
+import sys
+import MySQLdb
+
+host = 'localhost'
+port = 3306
+user = sys.argv[1]
+passwd = sys.argv[2]
+database = sys.argv[3]
+
+db = MySQLdb.connect(host=host, port=port, user=user, passwd=passwd, db=database)
+
+cur = db.cursor()
+
+cur.execute('SELECT * FROM states WHERE name LIKE BINARY "N%" ORDER BY states.id ASC')
+
+states = cur.fetchall()
+
+for state in states:
+    print(state)
+db.close()
