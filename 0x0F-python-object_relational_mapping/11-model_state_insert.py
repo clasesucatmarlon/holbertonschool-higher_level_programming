ce8104660b3f99812660c84bcc47f1346d5299cf
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-08-08 18:53:31 -0500

    script that insert state to database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""
+script that insert state to database hbtn_0e_6_usa
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    host = 'localhost'
+    port = 3306
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@{}:{}/{}'.format(
+        user, passwd, host, port, database), pool_pre_ping=True)
+    """ Base.metadata.create_all(engine) """
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new = State(name="Louisiana")
+    session.add(new)
+    session.commit()
+    print("{}".format(new.id))
+    session.close()
