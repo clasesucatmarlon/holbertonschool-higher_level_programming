877a517165b8d3fb6a2af69333f7cee5b02b40df
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-08-07 19:43:47 -0500

    script that takes in an argument and displays all values in the states table

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+# script that takes in an argument and displays all values in the states table
+# of hbtn_0e_0_usa where name matches the argument
+
+import sys
+import MySQLdb
+
+host = 'localhost'
+port = 3306
+user = sys.argv[1]
+passwd = sys.argv[2]
+database = sys.argv[3]
+name = sys.argv[4]
+
+db = MySQLdb.connect(host=host, port=port, user=user, passwd=passwd, db=database)
+
+cur = db.cursor()
+
+cur.execute('SELECT * FROM states WHERE name = "{}" ORDER BY states.id ASC'.format(name))
+
+states = cur.fetchall()
+
+for state in states:
+    print(state)
+db.close()
