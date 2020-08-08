9d672d1bf8ab581e612d19097512ffdbb59e3046
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-08-08 12:00:01 -0500

    add main

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,25 +1,37 @@
 #!/usr/bin/python3
-# script that takes in an argument and displays all values in the states table
-# of hbtn_0e_0_usa where name matches the argument
-
+"""
+script that takes in an argument and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument
+"""
 import sys
 import MySQLdb
 
-host = 'localhost'
-port = 3306
-user = sys.argv[1]
-passwd = sys.argv[2]
-database = sys.argv[3]
-name = sys.argv[4]
+def filterName():
+    """ filter to names
+    """
+    host = 'localhost'
+    port = 3306
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    database = sys.argv[3]
+    name = sys.argv[4]
 
-db = MySQLdb.connect(host=host, port=port, user=user, passwd=passwd, db=database)
+    db = MySQLdb.connect(host=host, port=port, user=user,
+                        passwd=passwd, db=database)
 
-cur = db.cursor()
+    cur = db.cursor()
 
-cur.execute('SELECT * FROM states WHERE name = "{:s}" ORDER BY states.id ASC'.format(name))
+    cur.execute('SELECT * FROM states WHERE name = "{:s}" ORDER BY\
+                states.id ASC'.format(name))
 
-states = cur.fetchall()
+    states = cur.fetchall()
 
-for state in states:
-    print(state)
-db.close()
+    for state in states:
+        print(state)
+    db.close()
+
+
+if __name__ == "__main__":
+    """ function main
+    """
+    filterName()
