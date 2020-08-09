27c658bb36196ed723f1fce5aed7fbf89d20fc65
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-08-08 19:00:37 -0500

    change name to starid=2

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""
+script that change name state to database hbtn_0e_6_usa
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    host = 'localhost'
+    port = 3306
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@{}:{}/{}'.format(
+        user, passwd, host, port, database), pool_pre_ping=True)
+    """ Base.metadata.create_all(engine) """
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    find_state = session.query(State).filter_by(id=2).first()
+    find_state.name = "New Mexico"
+    session.commit()
+    session.close()
