e57b4f73e02f6e82ae685a4bec4a1fcff2a5b541
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-08-08 19:20:13 -0500

    delete register for character a

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+"""
+script that deleted for a name state to database hbtn_0e_6_usa
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    host = 'localhost'
+    port = 3306
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@{}:{}/{}'.format(
+        user, passwd, host, port, database), pool_pre_ping=True)
+    """ Base.metadata.create_all(engine) """
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    filter = session.query(State).filter(State.name.contains("a"))
+    for data in filter:
+        session.delete(data)
+    session.commit()
+    session.close()
