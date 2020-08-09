d8148776700ba3b886a0d11d445badfa6a02d1b9
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-08-08 20:26:23 -0500

    Module for City class

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module for City class
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from model_state import Base, State
+
+
+class City(Base):
+    """ defines City class
+    """
+    __tablename__ = "cities"
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+    state = relationship("State")
