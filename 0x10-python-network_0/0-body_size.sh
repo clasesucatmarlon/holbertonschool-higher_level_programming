4582c78ee0d175c6f441a5d779a4255829a55912
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-08-18 11:45:51 -0500

    script that takes in a URL, sends a request to that URL

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# coment
+# script that takes in a URL, sends a request to that URL,
+# and displays the size of the body of the response
 curl -sI "$1" | grep Contenten-Length | cut -d' ' -f2
