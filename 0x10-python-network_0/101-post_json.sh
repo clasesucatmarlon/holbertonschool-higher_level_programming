6a61fb7b4791862cbc648a32b522ce43c63e5582
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-08-18 16:02:16 -0500

    Task: 8. cURL a JSON file

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+# script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
+curl -sH "Content-Type: application/json" --data "@$1" "$2"
