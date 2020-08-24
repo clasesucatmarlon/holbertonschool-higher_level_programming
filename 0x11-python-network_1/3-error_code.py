3af2cc800dd517befa9eb60d3b0d8a71178aacfa
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-08-24 11:22:33 -0500

    Task: 3. Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""
+send request
+"""
+import urllib.request
+import urllib.error
+from sys import argv
+
+
+def sender():
+    """ function sender
+    """
+    try:
+        with urllib.request.urlopen(argv[1]) as response:
+            the_page = response.read()
+            print(the_page.decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
+
+
+if __name__ == "__main__":
+    sender()
