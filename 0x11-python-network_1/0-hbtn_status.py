eba694c5e82299b15a8a977da629533272f14e67
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-08-24 11:09:11 -0500

    Task: 1. Response header value #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+fetcher https://intranet.hbtn.io/status
+"""
+import urllib.request
+
+
+def fetcher():
+    """ fetcher page
+    """
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode("utf-8")))
+
+
+if __name__ == "__main__":
+    fetcher()
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+fetcher https://intranet.hbtn.io/status passed into arg
+"""
+import urllib.request
+from sys import argv
+
+
+def fetcher():
+    """ fetcher page into arg
+    """
+    with urllib.request.urlopen(argv[1]) as response:
+        header = response.info()
+        print(header['X-Request-Id'])
+
+
+if __name__ == "__main__":
+    fetcher()
