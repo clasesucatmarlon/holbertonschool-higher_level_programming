d057ccb165913aae7ea1a2ee6e4f747a99558344
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-08-24 11:09:11 -0500

    Task: 2. POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""
+send post
+"""
+import urllib.parse
+import urllib.request
+from sys import argv
+
+
+def send_mail():
+    """ Send mail into arg
+    """
+    values = {'email': argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(argv[1], data)
+
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+        print(the_page.decode("utf-8"))
+
+
+if __name__ == "__main__":
+    send_mail()
