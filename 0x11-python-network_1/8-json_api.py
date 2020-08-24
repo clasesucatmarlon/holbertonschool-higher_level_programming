19d8b69f72a3040a5007d2f267a5188610d1a5d5
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-08-24 14:11:43 -0500

    Task: 8. Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""
+script that takes in a letter and sends a POSTrequest
+to http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+from sys import argv
+import requests
+
+
+def searchapi():
+    """ function for search api
+    """
+    if len(argv) < 2:
+        values = ""
+    else:
+        values = argv[1]
+
+    result = requests.post(
+        'http://0.0.0.0:5000/search_user', data={'q': values})
+
+    try:
+        data = result.json()
+        if data:
+            print("[{}] {}".format(data['id'], data['name']))
+        else:
+            print("No result")
+    except:
+        print("not a valid JSON")
+
+
+if __name__ == "__main__":
+    searchapi()
