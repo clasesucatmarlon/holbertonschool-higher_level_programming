2c80e0021954cc96033a14cb49bae889c8b9854e
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-08-24 12:24:21 -0500

    Task: 6. POST an email #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+check status
+"""
+import requests
+from sys import argv
+
+
+def post():
+    """ status
+    """
+    r1 = requests.post(argv[1], data={'email': argv[2]})
+    print(r1.text)
+
+
+if __name__ == "__main__":
+    post()
