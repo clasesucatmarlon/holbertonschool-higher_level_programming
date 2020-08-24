9928ca1def90c3075e679167adfeb6cc03293c70
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-08-24 12:40:28 -0500

    Task: 7. Error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""
+script that takes in a URL, sends a request to the URL
+and displays the body of the response
+"""
+from sys import argv
+import requests
+
+
+def codeerror():
+    """ function that return code error
+    """
+    result = requests.get(argv[1])
+    if result.status_code > 400:
+        print("Error code: {}".format(result.status_code))
+    else:
+        print(result.text)
+
+
+if __name__ == "__main__":
+    codeerror()
