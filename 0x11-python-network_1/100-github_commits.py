d62942f1f45ae26e5a4ab7cfd5e8376f4ad71663
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-08-24 17:16:57 -0500

    Task: 10. Time for an interview!

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""
+api commit
+"""
+import requests
+from sys import argv
+
+
+def api_commit():
+    """ api commit
+    """
+
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
+
+    req = requests.get(url)
+
+    result = req.json()[:10]
+
+    for commi in result:
+        sha = commi.get('sha')
+        commit = commi.get('commit')
+        author = commit.get('author')
+        name = author.get('name')
+        print("{}: {}".format(sha, name))
+
+
+if __name__ == "__main__":
+    api_commit()
