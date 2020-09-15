46c7039852f40d1cf0f2015eb6eb5febfac6ce32
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-09-15 13:34:35 -0500

    script that searches the second biggest integer in the list of arguments

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// script that searches the second biggest integer in the list of arguments
+
+const list = process.argv;
+list.splice(0, 2);
+const len = list.length;
+let value = 0;
+
+if (len === 0 || len === 1) {
+  console.log('0');
+} else {
+  list.sort((a, b) => a - b);
+  console.log(list);
+  value = list[len - 2];
+}
+console.log(value);
