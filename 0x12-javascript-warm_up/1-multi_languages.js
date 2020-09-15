dcad6bce433657739affaa017df2ba31c4a372e0
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-09-15 12:13:33 -0500

    Script thah display three lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 // Script thah display three lines
 
-console.log("C is fun\nPython is cool\nJavascript is amazing");
+console.log('C is fun\nPython is cool\nJavascript is amazing');
