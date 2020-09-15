566212e8a662b8e5cb6aeb83d1b88277e8437ce6
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-09-15 14:12:51 -0500

    Write a function that executes x times a function

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Write a function that executes x times a function
+
+function callMeMoby (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
+}
+
+module.exports = { callMeMoby };
