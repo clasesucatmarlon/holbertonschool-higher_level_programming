ac02fc763eec5f555ae4841b02f0f62c4cf3875c
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-09-15 13:22:13 -0500

    script that prints 3 lines into array

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// script that prints 3 lines: (like 1-multi_languages.js) but by using
+// an array of string and a loop
+
+const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const len = myArray.length;
+
+for (let i = 0; i < len; i++) {
+  console.log(myArray[i]);
+}
