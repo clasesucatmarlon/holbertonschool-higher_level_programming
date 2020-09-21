226e0de6699c1261e33235edad2e29d226a49471
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-09-21 12:10:38 -0500

    script that imports an array and computes a new array

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+// Write a script that imports an array and computes a new array
+
+const list = require('./100-data.js').list;
+
+const ar = list.map(function (x, i) {
+  return x * i;
+});
+
+console.log(list);
+console.log(ar);
