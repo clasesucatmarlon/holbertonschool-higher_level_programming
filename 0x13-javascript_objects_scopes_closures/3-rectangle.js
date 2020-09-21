ec1b67ab25c6151e514294144dbafdfbd1545ad1
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-09-21 10:22:20 -0500

    Create an instance method called print() that prints the rectangle using the character X

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+/* Create an instance method called print()
+that prints the rectangle using the character X
+ */
+
+class Rectangle {
+  constructor (w, h) {
+    if (isNaN(w) || isNaN(h) || w <= 0 || h <= 0) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;
