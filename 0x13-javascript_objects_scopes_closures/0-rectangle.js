d90da1582ad7a3812b1b8c8bd4dd7f0f4f2dfd13
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-09-21 09:55:59 -0500

    Empty class Rectangle that defines a rectangle

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+// Write an empty class Rectangle that defines a rectangle
+
+class Rectangle {
+  /* constructor(height, width) {
+  this.height = height;
+  this.width = width;
+  } */
+}
+
+module.exports = Rectangle;
