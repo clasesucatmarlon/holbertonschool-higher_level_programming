758a3f0d45cdd2c3b2f29d20c7878be6fc413c06
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-09-21 14:22:49 -0500

    script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+/* script that imports a dictionary of occurrences by
+user id and computes a dictionary of user ids by occurrence. */
+
+const dict = require('./101-data.js').dict;
+
+const d = {};
+
+for (const k in dict) {
+  if (d[dict[k]] === undefined) {
+    d[dict[k]] = [];
+  }
+  d[dict[k]].push(k);
+}
+console.log(d);
