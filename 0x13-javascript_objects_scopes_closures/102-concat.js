4ab6837c3fce03a784cddc5ca5ea59e319d3f432
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-09-21 15:00:10 -0500

    script that concats 2 files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -7,4 +7,4 @@ const f1 = fs.readFileSync(process.argv[2], 'utf8');
 const f2 = fs.readFileSync(process.argv[3], 'utf8');
 const f3 = f1 + f2;
 
-fs.writeFile(process.argv[4], f3);
+fs.writeFileSync(process.argv[4], f3);
