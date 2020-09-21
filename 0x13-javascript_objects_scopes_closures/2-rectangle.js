a482db2de64c99779cf56170bdae3928e87f0f76
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-09-21 10:14:06 -0500

    Empty class Rectangle that defines a rectangle - constructor take 2 arguments w and h with conditions

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+/* Write an empty class Rectangle that defines a rectangle
+- The constructor must take 2 arguments w and h
+- Initialize the instance attribute width with the value of w
+- Initialize the instance attribute height with the value of h
+- If w or h is equal to 0 or not a positive integer, create an empty object
+- If w or h is equal to 0 or not a positive integer, create an empty object
+*/
+
+class Rectangle {
+  constructor (w, h) {
+    if (isNaN(w) || isNaN(h) || w <= 0 || h <= 0) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
