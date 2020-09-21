622519a0de5ad25ed77731c0defa8daf35af47dd
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-09-21 10:30:43 -0500

    create instances rotate and double

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,34 @@
 #!/usr/bin/node
+/* - Create an instance method called rotate()
+  that exchanges the width and the height of the rectangle
+- Create an instance method called double()
+  that multiples the width and the height of the rectangle by 2 */
+
+class Rectangle {
+  constructor (w, h) {
+    if (isNaN(w) || isNaN(h) || w <= 0 || h <= 0) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const aux = this.width;
+    this.width = this.height;
+    this.height = aux;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
