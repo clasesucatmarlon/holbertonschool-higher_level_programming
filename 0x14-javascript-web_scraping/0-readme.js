9be9e062a66f1f608001482029fdd0a6d5a38299
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-09-22 10:49:54 -0500

    0x14. Javascript - Web scraping

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// print content au file
+
+const fs = require ('fs');
+
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  }
+   else {
+     console.log (data);
+   }
+});
