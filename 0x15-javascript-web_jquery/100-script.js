3bf1ec88ba084c21f04dd14d1b1d9bad7f6874fc
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-09-24 18:32:24 -0500

    updates the text color of the HEADER HTML tag to red

--- 0x15-javascript-web_jquery/100-script.js
+++ 0x15-javascript-web_jquery/100-script.js
@@ -1,1 +1,4 @@
-#!/usr/bin/node
+// updates the text color of the HEADER HTML tag to red 
+document.addEventListener('DOMContentLoaded', function (event) { 
+  document.querySelector('HEADER').style.color = '#FF0000'; 
+});
