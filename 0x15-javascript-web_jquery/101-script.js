72592720aea5669ab6f3183752a7dbd88d3e4dbb
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-09-24 18:34:51 -0500

    adds, removes, and clears LI elements from a list

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -1,1 +1,16 @@
-#!/usr/bin/node
+// adds, removes, and clears LI elements from a list
+$(document).ready(function () {
+  const item = "<li>Item</li>";
+
+  $('#add_item').click(function () {
+    $('.my_list').append(item);
+  });
+
+  $('#remove_item').click(function () {
+    $('li:last').remove();
+  });
+
+  $('#clear_list').click(function () {
+    $('ul').empty();
+  });
+});
