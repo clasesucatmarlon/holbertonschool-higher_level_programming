b88de40119c5305c9538f1b39f24cb7821767c90
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-09-24 11:08:20 -0500

    updates the text of the HTML tag HEADER

--- 0x15-javascript-web_jquery/6-script.js
+++ 0x15-javascript-web_jquery/6-script.js
@@ -1,1 +1,5 @@
-#!/usr/bin/node
+// Javascript script that updates the text of the HTML tag HEADER
+// to “New Header!!!” when the user clicks on DIV#update_header
+$('#update_header').click(function () {
+  $('HEADER').text('New Header!!!');
+});
