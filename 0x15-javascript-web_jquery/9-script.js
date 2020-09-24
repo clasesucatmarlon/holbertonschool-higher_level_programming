a0d2ad274c8d96fb224bc35b8217248f35d74e18
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-09-24 11:41:33 -0500

    script that fetches from url and displays value of hello

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,1 +1,6 @@
-#!/usr/bin/node
+// Javascript script that fetches from
+// https://fourtonfish.com/hellosalut/?lang=fr and displays
+// the value of hello from that fetch in the HTML’s tag DIV#hello.
+$.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data) {
+  $('#hello').text(data.hello);
+});
